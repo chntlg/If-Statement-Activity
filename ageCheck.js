354fec925099c4f9e77adb2acaca6ed47aded1db
chntlg <chantalguerrero04@gmail.com>
2024-10-10 22:36:38 +0800

    Initial commit with ageCheck.js

---
+++ ageCheck.js
@@ -0,0 +1,14 @@
+//Prompt the user to enter their age
+
+function askAge(){
+   let age = prompt("Please enter your age: ");
+    if (age < 13) {
+        alert("You are a child.");
+    } else if (age >= 13 && age <= 19) {
+        alert("You are a teen.");
+    } else if (age >= 20) {
+        alert("You are an adult.");
+    } 
+}
+
+
